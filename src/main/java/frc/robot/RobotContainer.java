fc93e65159a238a582ad06c0f0ee57a1d91e3219
Vatan Aksoy Tezer <vatanaksoytezer@gmail.com>
2021-01-10 15:13:30 +0300

    Initial commit

---
+++ src/main/java/frc/robot/RobotContainer.java
@@ -0,0 +1,36 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Constants.JoystickConstants;
+
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+
+  
+  public Joystick m_driverController = new Joystick(JoystickConstants.kDriverControllerPort);
+  public Joystick m_operatorController = new Joystick(JoystickConstants.kOperatorControllerPort);
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the button bindings
+    configureButtonBindings();
+  }
+
+  private void configureButtonBindings() {
+
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  public Command getAutonomousCommand() {
+    // An ExampleCommand will run in autonomous
+    return null;
+  }
+}
